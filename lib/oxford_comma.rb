d198d5491452a2a93813152152c2c8e970eb4673
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 14:43:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,11 @@
 def oxford_comma(array)
   
   if array.length == 1
-    array.each do |element|
-      puts element
-    end
+    puts array[0]
   elsif array.length == 2 
-    array.each do |element|
-      puts element.join(" and ")
-    end
+    
   else
-    final_element = array[-1]
-    array.pop
-    array.each do |element|
-      puts element.join(", ") + "and #{final_element}"
-    end
+    
   end
     
 end
