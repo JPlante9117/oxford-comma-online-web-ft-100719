cae86b383a429e6522c455463e336ed05971607d
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 14:43:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   
   if array.length == 1
     array.each do |element|
-      puts element.join
+      puts element
     end
   elsif array.length == 2 
     array.each do |element|
