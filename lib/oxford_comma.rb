1ea816373aa072a27fc05b4e728ea7f64cd0126a
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-07 14:47:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  
+  if array.length == 1
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+    final_element = array[-1]
+    array.pop
+    return array.join(", ") << ", and #{final_element}"
+  end
+    
 end
